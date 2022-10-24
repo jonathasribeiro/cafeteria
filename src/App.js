3d6e8812e0ca0196e2c92a46c7797955385fbf3c
jonathasribeiro <jonathasribeiroreal@gmail.com>
2022-10-24 15:30:35 -0300

    feat: remove warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
-import * as firebase from 'firebase';
 
 import './config/ReactotronConfig';
 
