897c7d305bb1add8832bee37a781458718867318
jonathasribeiro <jonathasribeiroreal@gmail.com>
2026-07-07 13:56:16 -0300

    security: move Firebase config to environment variables

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
 import App from './App';
+import { firebaseImpl } from './util/firebaseUtils';
 
-const config = {
-  apiKey: 'AIzaSyAgg_yDXt1Qoutu7hcOdXXOrFhiVBHjWeE',
-  authDomain: 'start-cbaa5.firebaseapp.com',
-  databaseURL: 'https://start-cbaa5.firebaseio.com',
-  projectId: 'start-cbaa5',
-  storageBucket: 'start-cbaa5.appspot.com',
-  messagingSenderId: '633975796254',
-  appId: '1:633975796254:web:0c0205b34d2c6a4e38740f',
-  measurementId: 'G-CYK469N5JS',
-};
-
-firebase.initializeApp(config);
-firebase.database();
+firebaseImpl.database();
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/util/firebaseUtils.js
+++ src/util/firebaseUtils.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAgg_yDXt1Qoutu7hcOdXXOrFhiVBHjWeE',
-  authDomain: 'start-cbaa5.firebaseapp.com',
-  databaseURL: 'https://start-cbaa5.firebaseio.com',
-  projectId: 'start-cbaa5',
-  storageBucket: 'start-cbaa5.appspot.com',
-  messagingSenderId: '633975796254',
-  appId: '1:633975796254:web:0c0205b34d2c6a4e38740f',
-  measurementId: 'G-CYK469N5JS',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const firebaseImpl = firebase.initializeApp(config);
